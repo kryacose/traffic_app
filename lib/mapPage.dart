3c2880b365d1bb28a2aa2f1cf0f735d7c4f107b8
kryacose <kuriakose.el@gmail.com>
2020-05-22 01:01:43 +0530

    Live Location

--- lib/mapPage.dart
+++ lib/mapPage.dart
@@ -1,34 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+// import 'package:geolocator/geolocator.dart';
+
+
 
 class MapPage extends StatefulWidget {
   MapPage({Key key}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   @override
   _MapPageState createState() => _MapPageState();
 }
 
 class _MapPageState extends State<MapPage> {
-  GoogleMapController mapController;
-  final LatLng _center = const LatLng(45.521563, -122.677433);
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
-  }
 
-  // void _incrementCounter() {
-  //   setState(() {
-  //     _counter++;
+  // void _getCurrentLocation() {
+  //   final Geolocator geolocator = Geolocator()..forceAndroidLocationManager;
+
+  //   geolocator
+  //       .getCurrentPosition(desiredAccuracy: LocationAccuracy.best)
+  //       .then((Position position) {
+  //     setState(() {
+  //       print(position);
+  //       _center =  LatLng(position.latitude, position.longitude);
+
+  //     });
+  //   }).catchError((e) {
+  //     print(e);
   //   });
   // }
+
+  GoogleMapController mapController;
+  LatLng _center =  LatLng(10.02 , 76.35);
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+
+  }
+
+
+  
 
   @override
   Widget build(BuildContext context) {
@@ -36,8 +45,11 @@ class _MapPageState extends State<MapPage> {
       onMapCreated: _onMapCreated,
       initialCameraPosition: CameraPosition(
         target: _center,
-        zoom: 11.0,
+        zoom: 16.0,
       ),
+      myLocationButtonEnabled: true,
+      myLocationEnabled: true,
     );
   }
 }
+
