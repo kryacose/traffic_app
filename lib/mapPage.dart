f251b4364e2ae3f66030aaa28387d4cea378da1e
kryacose <kuriakose.el@gmail.com>
2020-05-24 17:27:16 +0530

    Custom Markers

--- lib/mapPage.dart
+++ lib/mapPage.dart
@@ -1,8 +1,20 @@
+import 'dart:typed_data';
+import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'dart:ui' as ui;
 // import 'package:geolocator/geolocator.dart';
 
-
+Future<Uint8List> getBytesFromAsset(String path, int width) async {
+  ByteData data = await rootBundle.load(path);
+  ui.Codec codec = await ui.instantiateImageCodec(data.buffer.asUint8List(),
+      targetWidth: width);
+  ui.FrameInfo fi = await codec.getNextFrame();
+  return (await fi.image.toByteData(format: ui.ImageByteFormat.png))
+      .buffer
+      .asUint8List();
+}
 
 class MapPage extends StatefulWidget {
   MapPage({Key key}) : super(key: key);
@@ -12,44 +24,59 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
+  BitmapDescriptor markerIcon;
+  Set<Marker> _markers = {};
+  double _zoom = 16.0;
 
-  // void _getCurrentLocation() {
-  //   final Geolocator geolocator = Geolocator()..forceAndroidLocationManager;
-
-  //   geolocator
-  //       .getCurrentPosition(desiredAccuracy: LocationAccuracy.best)
-  //       .then((Position position) {
-  //     setState(() {
-  //       print(position);
-  //       _center =  LatLng(position.latitude, position.longitude);
-
-  //     });
-  //   }).catchError((e) {
-  //     print(e);
-  //   });
-  // }
-
-  GoogleMapController mapController;
-  LatLng _center =  LatLng(10.02 , 76.35);
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
-
+  @override
+  void initState() {
+    super.initState();
+    setCustomMapPin();
   }
 
+  //Load custom Marker from assets, set size here
+  void setCustomMapPin() async {
+    Uint8List markerImage =
+        await getBytesFromAsset('assets/circle.png', pow(_zoom,0.5).toInt()*30);
+    markerIcon = BitmapDescriptor.fromBytes(markerImage);
+  }
 
-  
+  void updateMarkers() {
+    _markers.add(Marker(
+        markerId: MarkerId("marker_id"), position: _center, icon: markerIcon,anchor: Offset(0.5,0.5)));
+  }
+
+  //Code to initialize the map
+  GoogleMapController mapController;
+  LatLng _center = LatLng(10.02, 76.35);
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+    setState(() {
+      updateMarkers();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return GoogleMap(
       onMapCreated: _onMapCreated,
+      markers: _markers,
       initialCameraPosition: CameraPosition(
         target: _center,
-        zoom: 16.0,
+        zoom: _zoom,
       ),
       myLocationButtonEnabled: true,
       myLocationEnabled: true,
+      onCameraIdle: () {
+        setState(() {
+          mapController.getZoomLevel().then((value) {
+            _zoom = value;
+            setCustomMapPin();
+            updateMarkers();
+            print(_zoom);
+          });
+        });
+      },
     );
   }
 }
-
